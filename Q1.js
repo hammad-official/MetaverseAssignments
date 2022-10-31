c664ed02d23ff59f031775a9b6f86c2db0d9e664
Muhammad Hammad <letstalkhammad@gmail.com>
2022-10-31 18:01:23 +0500

    Solved Questions

---
+++ Q1.js
@@ -0,0 +1,8 @@
+
+
+
+// Question 1
+let person_name = "Muhammad Hammad";
+let message = "Hello " + person_name + ", would you like to learn JAVASCRIPT Today?";
+console.log(message);
+alert(message);
